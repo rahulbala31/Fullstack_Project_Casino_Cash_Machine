6cbd49943fb2ebe2e8b542e0b6845e64c5a21828
rahulbala31 <rahulbalakrishnan3131@gmail.com>
2026-01-31 08:55:07 +0530

    Delete src directory

--- src/main/java/com/example/cashmachinebackend/dto/QRCodeRequest.java
+++
@@ -1,8 +0,0 @@
-package com.example.cashmachinebackend.dto;
-
-public class QRCodeRequest {
-    private String qrCode;
-
-    public String getQrCode() { return qrCode; }
-    public void setQrCode(String qrCode) { this.qrCode = qrCode; }
-}
